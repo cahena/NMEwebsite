2eba721db241276ed341043e2275a5176fd0863a
cahena <cahena@oregonstate.edu>
2024-09-07 12:53:12 -0700

    adding grid for history who, when, wherer

--- matching.js
+++ matching.js
@@ -1,1 +1,17 @@
 //alert("test");
+
+function fetchJSONData() { //https://www.geeksforgeeks.org/read-json-file-using-javascript/
+    fetch("/recruitment-data.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
