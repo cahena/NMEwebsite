07eace6f7f0a97eb1d960659b0b91276bbc6e7e8
cahena <cahena@oregonstate.edu>
2024-09-08 15:19:52 -0700

    js is reading and displaying to html

--- matching.js
+++ matching.js
@@ -1,17 +1,35 @@
 //alert("test");
 
-function fetchJSONData() { //https://www.geeksforgeeks.org/read-json-file-using-javascript/
-    fetch("/recruitment-data.json")
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error
-                    (`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) => 
-              console.log(data))
-        .catch((error) => 
-               console.error("Unable to fetch data:", error));
+
+// Create a FileReader object:
+const reader = new FileReader();
+
+// (MAIN) Event listener that stores chosen file info and displays results of program.
+reader.onload = function(event) { 
+    alert("inside");
+    // Parse the JSON string into a JavaScript object
+    const jsonData = event.target.result;
+    const data = JSON.parse(jsonData);
+
+    console.log("Test Data: ", data);
+
+
+    // Display results: 
+    const resultsDiv = document.getElementById("results");
+
+    // Bigs Pairs:
+    resultsDiv.innerHTML = "<h3>Big Pairs </h3>";
+    // Littles Pairs:
+    resultsDiv.innerHTML += "<h3>Little Pairs </h3>";
 }
-fetchJSONData();
+
+// Event listener for handling a file read error.
+reader.onerror = function(event) {
+    console.error("Error from reading the file:", event.target.error);
+};
+
+// Event listener that reads the file as text and calls reader.onload = function(event)
+document.getElementById('jsonFileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    reader.readAsText(file);
+});
